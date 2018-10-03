c3dbdc2a636b4c4fa8e0feb88bcda0646741ff6e
Dennis Værum <dvarum12@users.noreply.github.com>
2018-10-03 23:32:23 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 source config.sh
 
+DIR=${pwd}
+IMAGES="$DIR/images/"
+
+mkdir -p images
+
 wget https://downloads.lede-project.org/snapshots/targets/ar71xx/tiny/openwrt-imagebuilder-ar71xx-tiny.Linux-x86_64.tar.xz
 tar -xf openwrt-imagebuilder-ar71xx-tiny.Linux-x86_64.tar.xz
 cd openwrt-imagebuilder-ar71xx-tiny.Linux-x86_64.tar.xz
 make image PROFILE="$PROFILE" PACKAGES="$PACKAGES"
+cp build_dir/target-mips_24kc_musl/linux-ar71xx_tiny/tmp/* "$IMAGES"

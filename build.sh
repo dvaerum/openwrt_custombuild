8444b6b585690da58e8dade6e946a85b51f56fa4
Dennis Værum <github@varum.dk>
2018-10-04 19:39:31 +0200

    updated build.sh, sha256sum.txt and .travis.yml

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ source config.sh
 DIR=$(pwd)
 IMAGES="$DIR/images"
 BUILDS="build_dir/target-mips_24kc_musl/linux-ar71xx_tiny/tmp"
+CHECKSUM="$DIR/sha256sum.txt"
 
 mkdir -p "$IMAGES"
 
@@ -13,19 +14,22 @@ tar -xf openwrt-imagebuilder-ar71xx-tiny.Linux-x86_64.tar.xz
 cd openwrt-imagebuilder-ar71xx-tiny.Linux-x86_64
 make image PROFILE="$PROFILE" PACKAGES="$PACKAGES"
 
-if [ "$checksum" -ne "$(cat sha256sum.txt)" ]; then
-  echo $checksum > sha256sum.txt
+tmp="$(cat $CHECKSUM)"
+if [ "$checksum" != "$tmp" ]; then
+  echo $checksum > "$CHECKSUM"
   cp build_dir/target-mips_24kc_musl/linux-ar71xx_tiny/tmp/* "$IMAGES"
-  
+
   git config user.email "$GIT_MAIL"
   git config user.name "$GIT_USER"
 
   git checkout master
-#  git add -A "$IMAGES"/*
-  git add -A "sha256sum.txt"
+  git add "$CHECKSUM"
   git commit -m "new updated images"
-  git tag -a "$(date +%Y-%m-%d_%H-%M-%S)" -m "new updated images"
-  
+#  git tag -a "$(date +%Y-%m-%d_%H-%M-%S)" -m "new updated images"
+  git tag -a "$(date +%Y-%m-%d)" -m "new updated images"
+
   git remote add origin-release "https://${GITHUB_TOKEN}@github.com/dvarum12/openwrt_custombuild" > /dev/null 2>&1
   git push --quiet --set-upstream origin-release master --tags
+
+  export NEW_BUILD=YES
 fi

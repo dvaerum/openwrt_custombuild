a26c8816dfb38946371022d1c396f646835750f1
Dennis Værum <github@varum.dk>
2018-10-04 18:23:37 +0200

    updated build.sh, config,sh and .travis.yml

--- build.sh
+++ build.sh
@@ -1,12 +1,31 @@
 source config.sh
 
-DIR=${pwd}
-IMAGES="$DIR/images/"
+DIR=$(pwd)
+IMAGES="$DIR/images"
+BUILDS="build_dir/target-mips_24kc_musl/linux-ar71xx_tiny/tmp"
 
-mkdir -p images
+mkdir -p "$IMAGES"
 
 wget https://downloads.lede-project.org/snapshots/targets/ar71xx/tiny/openwrt-imagebuilder-ar71xx-tiny.Linux-x86_64.tar.xz
+checksum=$(sha256sum openwrt-imagebuilder-ar71xx-tiny.Linux-x86_64.tar.xz)
+
 tar -xf openwrt-imagebuilder-ar71xx-tiny.Linux-x86_64.tar.xz
-cd openwrt-imagebuilder-ar71xx-tiny.Linux-x86_64.tar.xz
+cd openwrt-imagebuilder-ar71xx-tiny.Linux-x86_64
 make image PROFILE="$PROFILE" PACKAGES="$PACKAGES"
-cp build_dir/target-mips_24kc_musl/linux-ar71xx_tiny/tmp/* "$IMAGES"
+
+if [ "$checksum" -ne "$(cat sha256sum.txt)" ]; then
+  echo $checksum > sha256sum.txt
+  cp build_dir/target-mips_24kc_musl/linux-ar71xx_tiny/tmp/* "$IMAGES"
+  
+  git config user.email "$GIT_MAIL"
+  git config user.name "$GIT_USER"
+
+  git checkout master
+#  git add -A "$IMAGES"/*
+  git add -A "sha256sum.txt"
+  git commit -m "new updated images"
+  git tag -a "$(date +%Y-%m-%d_%H-%M-%S)" -m "new updated images"
+  
+  git remote add origin-release "https://${GITHUB_TOKEN}@github.com/dvarum12/openwrt_custombuild" > /dev/null 2>&1
+  git push --quiet --set-upstream origin-release master --tags
+fi
--- config.sh
+++ config.sh
@@ -1,2 +1,5 @@
 PROFILE="tl-wr841-v8"
 PACKAGES="-wpad-mini -ppp -ppp-mod-pppoe -ip6tables -odhcp6c -kmod-ipv6 -kmod-ip6tables wpad luci"
+
+GIT_MAIL="travis@travis-ci.org"
+GIT_USER="Travis CI"
